4698aa60780f6c7de093f84298bb219e57821091
Nuttapolxz <nuttapolt62@nu.ac.th>
2021-11-04 17:17:30 +0700

    [U4] Book details page

--- u4-databases/app.js
+++ u4-databases/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const expHbs = require('express-handlebars')
 const { json, urlencoded } = require('body-parser')
+const { bookDetails } = require('./features/book-controller')
+
 const app = express()
 // Templates
 app.set('views', './views')
@@ -10,11 +12,15 @@ extname: '.hbs',
 defaultLayout: false,
 partialsDir: ['./views/partials', './views/layouts']
 }))
+
 // Middleware
 app.use(express.static('public'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
+
 // Routes
+app.get('/books/:isbn13', bookDetails)
+
 // TODO later
 const PORT = 3000
 app.listen(PORT,
