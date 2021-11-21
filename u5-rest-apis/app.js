55a7ae346e4aeaea10d9981ff0add09fe57fbe33
Nuttapolxz <nuttapolt62@nu.ac.th>
2021-11-22 00:47:12 +0700

    [U5] one book

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
-const { readBooks } = require('./features/book-controller')
+const { readBooks,readBook  } = require('./features/book-controller')
 const app = express()
 // Middleware
 app.use(express.static('public'))
@@ -9,4 +9,5 @@ app.use(urlencoded({ extended: false }))
 // Routes
 // TODO later
 app.get('/books', readBooks)
+app.get('/books/:isbn13', readBook)
 module.exports = { app }
