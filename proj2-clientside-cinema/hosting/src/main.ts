b2a17e5aa91ef9beb133dee9c9e5a22afe33d835
Nuttapolxz <nuttapolt62@nu.ac.th>
2021-11-27 00:53:08 +0700

    [MP2] Setup Deps

--- proj2-clientside-cinema/hosting/src/main.ts
+++ proj2-clientside-cinema/hosting/src/main.ts
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+import { firestorePlugin } from 'vuefire'
+import Buefy from 'buefy'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'buefy/dist/buefy.css'
+import '@mdi/font/css/materialdesignicons.min.css'
+import './main.scss'
 
 Vue.config.productionTip = false
+Vue.use(firestorePlugin)
+Vue.use(Buefy)
 
 new Vue({
   router,
